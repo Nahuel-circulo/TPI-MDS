62f4b93ca81b5209069a32605b7285176e620f8b
Nahuelnp <nahuelyad@gmail.com>
2022-08-27 16:25:44 -0300

    modificaciond el footer

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,40 +1,30 @@
 <template>
   <v-footer class="footer font-inter">
     <div class="footer__container">
-      <router-link class="footer__logo" to="/">
-        <!-- <img :src="require('../assets/logo.svg')" alt="" /> -->
-      </router-link>
-      <div class="footer__information">
-        <div class="footer__location">
-          <h3 class="footer__location-title">Sede Central Resistencia</h3>
-          <p class="footer__location-address">
-            <v-icon class="footer__location-icon">mdi-map-marker</v-icon>French
-            405, Resistencia Chaco.
-          </p>
-          <p class="footer__location-phone">
-            <v-icon class="footer__location-icon">mdi-phone</v-icon>(011)
-            5544-7000
-          </p>
+      <div class="footer__info">
+        <div style="max-width: 485px">
+          <v-img
+            class="footer__info-logo"
+            :src="require('@/assets/logo.svg')"
+          />
         </div>
-        <div class="footer__social-media">
-          <a style="{{'text-decoration': 'none';}}"
-            href="https://www.linkedin.com/company/powgen-diesel-s-a/"
-            target="_blank"
-            ><v-icon class="footer__location-icon">mdi-twitter</v-icon></a>
-          <a style="{{'text-decoration': 'none';}}" href="https://www.facebook.com/powgensa" target="_blank"
-            ><v-icon class="footer__location-icon">mdi-facebook</v-icon></a>
-          <a style="{{'text-decoration': 'none';}}" href="https://www.instagram.com/powgenargentina/" target="_blank"
-            ><v-icon class="footer__location-icon">mdi-instagram</v-icon></a>
-        </div>
-      </div>
-
-      <div class="footer__copyright">
-        <p>© 2022 Sentidos® | Todos los derechos reservados.</p>
-
-        <p>
-          Creado por DevStation
-          <!-- <span> <img src="/icons/footer/CirculoLogo.svg" alt="" /></span> -->
+        <p style="max-width: 485px; text-align: center">
+          Copyright © 2022 Sentidos, Inc.
         </p>
+        <div class="d-flex justify-space-between flex-wrap py-2">
+          <p>
+            <span class="text-divider">|</span>
+            Política de privacidad
+            <span class="text-divider">|</span>
+            Condiciones de Servicio
+            <span class="text-divider">|</span>
+          </p>
+          <p>
+            <span class="text-divider">|</span>
+            Desarrollado por DevStation
+            <span class="text-divider">|</span>
+          </p>
+        </div>
       </div>
     </div>
   </v-footer>
